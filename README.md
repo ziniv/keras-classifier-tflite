bab9509bac7f360f62a0e9a9e370f032c820bec3
yj7840 <lgenney@gmail.com>
2023-03-07 16:57:51 +0900

    modified convert_tflite.py add requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Keras classifier (with converting h5 to tflite)
+
+## 1. setting  
+    pip install -r requirements.txt  
+
+    You can choice cifar10 datasets, if you want to train classification model quickly because it is small. ImageNet, on the other hand, it is a much larger dataset. ImageNet has become a standard benchmark dataset for evaluating the performance of image classification.
+
+    python -m prepare_dataset --name tinyimagenet --extract True --output ./dataset  
+
+    python -m prepare_dataset --name cifar --extract True --output ./dataset  
+
+    python -m prepare_dataset --name mnist --extract True --output ./dataset   
+
+## 2. Train  
+    python -m train --name resnet18 --dataset tinyimagenet --epochs 300 --batch 128 --logdir ./logs --weight_decay 0.0005 --save True  
+    python -m train --name resnet18 --dataset cifar --epochs 300 --batch 128 --logdir ./logs --weight_decay 0.0005 --save True  
+
+
+
--- convert_tflite.py
+++ convert_tflite.py
@@ -1,25 +1,44 @@
-import tensorflow.keras as keras
+import os
 import tensorflow as tf
-from keras_radam import RAdam
 import cv2
 import glob
 import random
 import numpy as np
 import argparse
 
+def get_resnet18(input_shape, num_classes, name):
+    import keras
+    from keras_resnet import models
+    from keras.models import Model
+    from keras.layers import Dense, GlobalAveragePooling2D
+    input_tensor = keras.layers.Input(input_shape)
+    if name == 'resnet18':
+        base_model = models.ResNet18(inputs = input_tensor, classes=num_classes, include_top=False)
+        x = base_model.output[-1]
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+        predictions = Dense(num_classes, activation='softmax')(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
+        return model
+    
+    
+
 if __name__ == "__main__":
+    from tensorflow import keras
     parser = argparse.ArgumentParser(description="convert h5 to tflite")
     parser.add_argument('--name', default='resnet18', type=str)
-    parser.add_argument('--input', default='./saves_models/resnet18-cifar_final.h5', type=str)
-    parser.add_argument('--output', default='./saves_models/', type=str)
+    parser.add_argument('--input', default='./saved_models/resnet18-cifar_final.h5', type=str)
+    parser.add_argument('--output', default='./saved_models/', type=str)
     parser.add_argument('--is_quan', default=True, type=bool)
     
     args = parser.parse_args()
     
-    model = keras.models.load_model(args.input)
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    tflite_model = converter.convert()
     filename = args.input.split('/')[-1].split('.h5')[0]
-    print(filename)
-    # with open(args)
+    restore_filename = os.path.join(args.output, filename+'_restore.h5')
+    model = get_resnet18((32, 32, 3), 10, 'resnet18')
+    model.load_weights(args.input)
+    model.save(restore_filename)
+    
+    model = keras.models.load_model(restore_filename, custom_objects = model)
+    
     
